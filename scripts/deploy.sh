bad9363beb1a6c60ef55ab49ff4c23a1d9f6af56
David Winter <i@djw.me>
2018-10-30 16:58:33 +0000

    Update deploy script to use different environment manifest files
    Have renamed CF apps to mirror the Github repo name with environment 
    suffixed.
    
    Currently tied to the `sandbox` space, but in future can be updated to 
    deploy to corresponding spaces once ready.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,18 @@
 
 set -e
 
+if [ -z "$1" ]; then
+  echo 'Provide an environment to deploy'
+  exit 1
+fi
+
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.cloud.service.gov.uk
-./cf auth "$CF_USER" "$CF_PASSWORD"
-./cf target -o "$CF_ORG" -s "$CF_SPACE"
+./cf auth ${CF_USER} ${CF_PASSWORD}
 
-cd WebApi && ../cf push
+# Use when we can safely deploy to the `staging` and `production` spaces
+# For now, fix to `sandbox` space
+# ./cf target -o ${CF_ORG} -s ${1}
+./cf target -o ${CF_ORG} -s sandbox
+
+./cf push -f deploy-manifests/${1}.yml
