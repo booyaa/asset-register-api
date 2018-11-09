edd587c5d9704bb7251ba87cd55abdeb67261f23
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-09 11:00:48 +0000

    Initial Asset model from screenshot

--- AcceptanceTest/HomesEngland/AssetTests.cs
+++ AcceptanceTest/HomesEngland/AssetTests.cs
@@ -64,6 +64,7 @@ namespace AssetRegisterTests.HomesEngland
                 Address = "1, The Pavement, Town",
                 SchemeID = "22",
                 AccountingYear = "1999"
+                SchemeId = 22,
 
             };
 
