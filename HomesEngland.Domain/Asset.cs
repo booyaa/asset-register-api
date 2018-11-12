d03ae11efde97a870cc442af08cd3c1967a7c6d1
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-12 11:04:59 +0000

    Test data helper

---
+++ HomesEngland.Domain/Asset.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace HomesEngland.Domain.Domain
+{
+    public class Asset
+    {
+        public string MonthPaid { get; set; }
+        public string AccountingYear { get; set; }
+        public string SchemeID { get; set; }
+
+        //Identifying Information
+        public string LocationLaRegionName { get; set; }
+        public string IMSOldRegion { get; set; }
+        public string NoOfBeds { get; set; }
+        //IMS Adress
+        public string Address { get; set; }
+        public string DevelopingRslName { get; set; }
+        //Completion Dates
+        public DateTime? CompletionDateForHpiStart { get; set; }
+        public DateTime? ImsActualCompletionDate { get; set; }
+        public DateTime? ImsExpectedCompletionDate { get; set; }
+        public DateTime? ImsLegalCompletionDate { get; set; }
+        public DateTime? HopCompletionDate { get; set; }
+        //Financial Information
+        public decimal? Deposit { get; set; }
+        public decimal? AgencyEquityLoan { get; set; }
+        public decimal? DeveloperEquityLoan { get; set; }
+        public decimal? ShareOfRestrictedEquity { get; set; }
+        //Calcuation
+        public int? DifferenceFromIMSExpectedCompletionToHopCompletionDate { get; set; }
+
+        public Dictionary<string, string> ToDictionary()
+        {
+            return new Dictionary<string, string> {
+                {"Address", Address },
+                {"SchemeID", SchemeID.ToString()},
+                {"AccountingYear", AccountingYear.ToString()}
+            };
+        }
+    }
+}
---
+++ TestHelper/TestData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Bogus;
+using HomesEngland.Domain.Domain;
+
+namespace TestHelper
+{
+    public static class TestData
+    {
+        public static class Domain
+        {
+            public static Asset GenerateAsset()
+            {
+                var random = new Random(0);
+
+                var faker = new Faker("en-gb");
+                var completionDateForHpiStart = faker.Date.Soon(random.Next(1, 15));
+                var imsActualCompletionDate = faker.Date.Soon(random.Next(30, 90));
+                var imsExpectedCompletionDate = faker.Date.Soon(random.Next(15, 90));
+                var hopCompletionDate = faker.Date.Soon(random.Next(15, 90));
+                var differenceFromImsExpectedCompletionToHopCompletionDate = (imsExpectedCompletionDate.Date - hopCompletionDate).Days;
+
+                var asset = new Faker<Asset>("en-gb")
+                    .RuleFor(property => property.AccountingYear, (fake, model) => random.Next(2018, 2020).ToString())
+                    .RuleFor(property => property.Address, (fake, model) => fake.Address.FullAddress())
+                    .RuleFor(property => property.NoOfBeds, (fake, model) => fake.Random.Int(1, 4).ToString())
+                    .RuleFor(property => property.NoOfBeds, (fake, model) => fake.Random.Int(1, 4).ToString())
+                    .RuleFor(property => property.DevelopingRslName, (fake, model) => fake.Company.CompanyName())
+                    .RuleFor(property => property.LocationLaRegionName, (fake, model) => fake.Address.County())
+                    .RuleFor(property => property.IMSOldRegion, (fake, model) => fake.Address.County())
+                    .RuleFor(property => property.MonthPaid, (fake, model) => fake.Date.Month())
+                    .RuleFor(property => property.SchemeID, (fake, model) => fake.IndexGlobal.ToString())
+                    .RuleFor(property => property.AgencyEquityLoan,(fake, model) => fake.Finance.Amount(5000m, 100000m))
+                    .RuleFor(property => property.CompletionDateForHpiStart, (fake, model) => completionDateForHpiStart)
+                    .RuleFor(property => property.ImsActualCompletionDate, (fake, model) => imsActualCompletionDate)
+                    .RuleFor(property => property.ImsExpectedCompletionDate, (fake, model) => imsExpectedCompletionDate)
+                    .RuleFor(property => property.HopCompletionDate, (fake, model) => hopCompletionDate)
+                    .RuleFor(property => property.DifferenceFromIMSExpectedCompletionToHopCompletionDate, (fake, model) => differenceFromImsExpectedCompletionToHopCompletionDate)
+                    .RuleFor(property => property.DeveloperEquityLoan, (fake, model) => null)
+                    .RuleFor(property => property.ShareOfRestrictedEquity, (fake, model) => fake.Finance.Amount(50,100))
+                    ;
+                return asset;
+            }
+        }
+    }
+}
