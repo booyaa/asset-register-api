4eb34623e001539265b2100ee3cd637eece1b6f4
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-12 08:09:19 +0000

    attempting to get a postgres docker image for testing going

---
+++ HomesEngland/Gateway/AssetGateway.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using HomesEngland.Boundary.Port;
+using HomesEngland.Domain;
+
+namespace HomesEngland.Gateway
+{
+    public class AssetGateway:IAssetGateway
+    {
+        private readonly IDbConnection _connection;
+
+        public AssetGateway(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public Task<int> AddAsset(Asset asset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Asset> GetAsset(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Asset[]> GetAssets(int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Asset[]> SearchAssets(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
