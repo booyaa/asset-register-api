fdc46200684cfb8acfeee8272d899cbe8b3fcaab
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-16 12:35:43 +0000

    update parameter names and add comments to make it clearer

--- HomesEngland.Gateway/PostgresDatabaseConnectionFactory.cs
+++ HomesEngland.Gateway/PostgresDatabaseConnectionFactory.cs
@@ -7,19 +7,25 @@ namespace HomesEngland.Gateway
 {
     public class PostgresDatabaseConnectionFactory : IDatabaseConnectionFactory
     {
-        public IDbConnection Create(string connectionString)
+        /// <summary>
+        /// Takes a //postgres://circleci:super-secret@localhost:5432/asset_register_api databaseUrl
+        /// </summary>
+        /// <param name="databaseUrl"></param>
+        /// <returns></returns>
+        public IDbConnection Create(string databaseUrl)
         {
-            var uri = new Uri(connectionString);
+            //postgres://circleci:super-secret@localhost:5432/asset_register_api
+            var uri = new Uri(databaseUrl);
             var splitUserInfo = uri.UserInfo.Split(':');
             var server = uri.Host;
             var port = uri.Port;
             var userId = splitUserInfo.ElementAtOrDefault(0);
             var password = splitUserInfo.ElementAtOrDefault(1);
             var database = uri.LocalPath.Substring(1);
+            //construct connection string in the format that it's required 
+            string ngpsqlConnectionString = $"Server={server};Port={port};User Id={userId};Password={password};Database={database};";
 
-            string connstring = $"Server={server};Port={port};User Id={userId};Password={password};Database={database};";
-
-            return new NpgsqlConnection(connstring);
+            return new NpgsqlConnection(ngpsqlConnectionString);
         }
     }
 }
