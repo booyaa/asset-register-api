d156dafbd63f2288d531c83c1bb4511037c8ee0d
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-16 14:10:52 +0000

    increase readability

--- HomesEngland.Gateway/PostgresDatabaseConnectionFactory.cs
+++ HomesEngland.Gateway/PostgresDatabaseConnectionFactory.cs
@@ -14,18 +14,22 @@ namespace HomesEngland.Gateway
         /// <returns></returns>
         public IDbConnection Create(string databaseUrl)
         {
-            //postgres://circleci:super-secret@localhost:5432/asset_register_api
+            var ngpsqlConnectionString = BuildConnectionStringFromUrl(databaseUrl);
+
+            return new NpgsqlConnection(ngpsqlConnectionString);
+        }
+
+        public string BuildConnectionStringFromUrl(string databaseUrl)
+        {
             var uri = new Uri(databaseUrl);
             var splitUserInfo = uri.UserInfo.Split(':');
             var server = uri.Host;
             var port = uri.Port;
             var userId = splitUserInfo.ElementAtOrDefault(0);
             var password = splitUserInfo.ElementAtOrDefault(1);
-            var database = uri.LocalPath.Substring(1);
-            //construct connection string in the format that it's required 
+            var database = uri.LocalPath.Substring(1); 
             string ngpsqlConnectionString = $"Server={server};Port={port};User Id={userId};Password={password};Database={database};";
-
-            return new NpgsqlConnection(ngpsqlConnectionString);
+            return ngpsqlConnectionString;
         }
     }
 }
