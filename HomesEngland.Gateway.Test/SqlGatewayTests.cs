3cc9a0c5fec1474214e7f8e6332ce248bf0ec88d
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-20 13:01:44 +0000

    Make tests specific to fields and have them be close to the datetime

--- HomesEngland.Gateway.Test/SqlGatewayTests.cs
+++ HomesEngland.Gateway.Test/SqlGatewayTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using HomesEngland.Domain;
 using HomesEngland.Gateway.Assets;
@@ -26,12 +27,29 @@ namespace HomesEngland.Gateway.Test
         {
             //arrange 
             var entity = TestData.Domain.GenerateAsset();
-
             var createdAsset = await _classUnderTest.CreateAsync(entity).ConfigureAwait(false);
             //act
             var readAsset = await _classUnderTest.ReadAsync(createdAsset.Id).ConfigureAwait(false);
+            
             //assert
-            readAsset.Should().BeEquivalentTo(entity);
+            readAsset.AccountingYear.Should().BeEquivalentTo(entity.AccountingYear);
+            readAsset.Address.Should().BeEquivalentTo(entity.Address);
+            readAsset.AgencyEquityLoan.Should().Be(entity.AgencyEquityLoan);
+            readAsset.CompletionDateForHpiStart.Should().BeCloseTo(entity.CompletionDateForHpiStart.Value, TimeSpan.FromMilliseconds(1.0));
+            readAsset.Deposit.Should().Be(entity.Deposit);
+            readAsset.DeveloperEquityLoan.Should().Be(entity.DeveloperEquityLoan);
+            readAsset.DevelopingRslName.Should().Be(entity.DevelopingRslName);
+            readAsset.DifferenceFromImsExpectedCompletionToHopCompletionDate.Should().Be(entity.DifferenceFromImsExpectedCompletionToHopCompletionDate);
+            readAsset.HopCompletionDate.Should().BeCloseTo(entity.HopCompletionDate.Value, TimeSpan.FromMilliseconds(1.0));
+            readAsset.ImsActualCompletionDate.Should().BeCloseTo(entity.ImsActualCompletionDate.Value, TimeSpan.FromMilliseconds(1.0));
+            readAsset.ImsExpectedCompletionDate.Should().BeCloseTo(entity.ImsExpectedCompletionDate.Value, TimeSpan.FromMilliseconds(1.0));
+            readAsset.ImsLegalCompletionDate.Should().BeCloseTo(entity.ImsLegalCompletionDate.Value, TimeSpan.FromMilliseconds(1.0));
+            readAsset.ImsOldRegion.Should().BeEquivalentTo(entity.ImsOldRegion);
+            readAsset.LocationLaRegionName.Should().BeEquivalentTo(entity.LocationLaRegionName);
+            readAsset.MonthPaid.Should().BeEquivalentTo(entity.MonthPaid);
+            readAsset.NoOfBeds.Should().BeEquivalentTo(entity.NoOfBeds);
+            readAsset.SchemeId.Should().BeEquivalentTo(entity.SchemeId);
+            readAsset.ShareOfRestrictedEquity.Should().Be(entity.ShareOfRestrictedEquity);
         }
     }
 }
