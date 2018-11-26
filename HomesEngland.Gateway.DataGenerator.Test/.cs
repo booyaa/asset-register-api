a71a9af360985fdc3b2d752de6a2b3a5c4d8cebc
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-26 10:35:28 +0000

    Splitting the generator out

---
+++ HomesEngland.Gateway.DataGenerator.Test/.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomesEngland.Gateway.DataGenerator.Test
+{
+    [TestClass]
+    public class InputSanitizerTests
+    {
+        private IInputSanitizer _classUnderTest;
+        public InputSanitizerTests(IInputSanitizer inputSanitizer)
+        {
+
+        }
+
+        [TestMethod]
+        public async Task GivenValidInput_DataGenerator_PassesCorrectNumberOfRecord()
+        {
+
+        }
+    }
+}
---
+++ HomesEngland.Gateway.DataGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HomesEngland.UseCase.Models;
+using Infrastructure.Api.Response.Validation;
+
+namespace HomesEngland.Gateway.DataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IInputSanitizer
+    {
+        
+    }
+
+    public class GenerateDataRequest : IRequest
+    {
+        public RequestValidationResponse Validate(IRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
