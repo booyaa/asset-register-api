dc070a4f1e3769dc132a5084947cbd58394e2287
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-11-28 09:54:52 +0000

    Validation tests

--- HomesEngland/UseCase/SearchAsset/Models/Validation/SearchAssetRequestValidator.cs
+++ HomesEngland/UseCase/SearchAsset/Models/Validation/SearchAssetRequestValidator.cs
@@ -8,7 +8,7 @@ namespace HomesEngland.UseCase.SearchAsset.Models.Validation
         {
             RuleFor(x => x).NotNull().WithMessage("Request must not be null");
             RuleFor(x => x.SchemeId).NotNull().GreaterThan(0).When(w=> string.IsNullOrEmpty(w.Address)).WithMessage("SchemeId must not be null and must be greater than 0.");
-            RuleFor(x => x.Address).NotNull().NotEmpty().When(w=> w.SchemeId.HasValue == false).WithMessage("SchemeId must not be null and must be greater than 0.");
+            RuleFor(x => x.Address).NotNull().NotEmpty().When(w=> w.SchemeId.HasValue == false).WithMessage("Address must not be null or empty.");
         }
     }
 }
