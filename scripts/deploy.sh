7761eaeaa2bd940f83021fe876a92dcbaa388099
Anthony King <anthony@madetech.com>
2018-12-03 14:23:27 +0000

    enable use of CORS  policies

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,10 +8,19 @@ if [ -z "$1" ]; then
 fi
 
 APP_NAME=${1}
+ENVIRONMENT_NAME="${1}"
 
 if [ "$APP_NAME" == "production" ]; then
   APP_NAME=${APP_NAME}-dark
 fi
+
+get_cors_origins() {
+  if [[ "${ENVIRONMENT_NAME}" == "production" ]]; then
+    echo "${FRONTEND_URI_PRODUCTION}"
+  else
+    echo "${FRONTEND_URI_STAGING}"
+  fi
+}
 
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.cloud.service.gov.uk
@@ -22,3 +31,4 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 ./cf push -f deploy-manifests/${APP_NAME}.yml
 ./cf set-env asset-register-api-${APP_NAME} circle_commit ${CIRCLE_SHA1}
 ./cf set-env asset-register-api-${APP_NAME} SENTRY_DSN ${SENTRY_DSN}
+./cf set-env asset-register-api-${APP_NAME} CorsOrigins "$(get_cors_origins)"
