51dcfdea9cf1f8f223f710903f95cdbbe81c4d48
Anthony King <anthony@madetech.com>
2018-12-03 14:46:02 +0000

    centralise conditional

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,21 +6,16 @@ if [ -z "$1" ]; then
   echo 'Provide an environment to deploy'
   exit 1
 fi
-
-APP_NAME=${1}
 ENVIRONMENT_NAME="${1}"
 
-if [ "$APP_NAME" == "production" ]; then
-  APP_NAME=${APP_NAME}-dark
+PRODUCTION_NAME="production"
+if [[ "${ENVIRONMENT_NAME}" == "${PRODUCTION_NAME}" ]]; then
+  APP_NAME="${ENVIRONMENT_NAME}-dark"
+  FRONTEND_URI="${FRONTEND_URI_PRODUCTION}"
+else
+  APP_NAME="${ENVIRONMENT_NAME}"
+  FRONTEND_URI="${FRONTEND_URI_STAGING}"
 fi
-
-get_cors_origins() {
-  if [[ "${ENVIRONMENT_NAME}" == "production" ]]; then
-    echo "${FRONTEND_URI_PRODUCTION}"
-  else
-    echo "${FRONTEND_URI_STAGING}"
-  fi
-}
 
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.cloud.service.gov.uk
@@ -31,4 +26,4 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 ./cf push -f deploy-manifests/${APP_NAME}.yml
 ./cf set-env asset-register-api-${APP_NAME} circle_commit ${CIRCLE_SHA1}
 ./cf set-env asset-register-api-${APP_NAME} SENTRY_DSN ${SENTRY_DSN}
-./cf set-env asset-register-api-${APP_NAME} CorsOrigins "$(get_cors_origins)"
+./cf set-env asset-register-api-${APP_NAME} CorsOrigins "${FRONTEND_URI}"
