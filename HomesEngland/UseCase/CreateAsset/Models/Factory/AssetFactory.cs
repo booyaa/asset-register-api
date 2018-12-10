48f2cd2be3868df8c364623c6c14288873304f67
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-12-10 16:30:44 +0000

    adjusting indexing

--- HomesEngland/UseCase/CreateAsset/Models/Factory/AssetFactory.cs
+++ HomesEngland/UseCase/CreateAsset/Models/Factory/AssetFactory.cs
@@ -36,41 +36,41 @@ namespace HomesEngland.UseCase.CreateAsset.Models.Factory
             decimal.TryParse(fields.ElementAtOrDefault(32), out var newAgencyPercentage);
             int.TryParse(fields.ElementAtOrDefault(33), out var invested);
             int.TryParse(fields.ElementAtOrDefault(34), out var month);
-            int.TryParse(fields.ElementAtOrDefault(35), out var calendarYear);
-            int.TryParse(fields.ElementAtOrDefault(36), out var row);
-            int.TryParse(fields.ElementAtOrDefault(37), out var col);
-            decimal.TryParse(fields.ElementAtOrDefault(38), out var hpiStart);
-            decimal.TryParse(fields.ElementAtOrDefault(39), out var hpiEnd);
-            decimal.TryParse(fields.ElementAtOrDefault(40), out var hpiPlusMinus);
-            decimal.TryParse(fields.ElementAtOrDefault(41), out var agencyPercentage);
-            decimal.TryParse(fields.ElementAtOrDefault(42), out var mortgageEffect);
-            decimal.TryParse(fields.ElementAtOrDefault(43), out var remainingAgencyCost);
-            decimal.TryParse(fields.ElementAtOrDefault(44), out var waEstimatedPropertyValue);
-            decimal.TryParse(fields.ElementAtOrDefault(45), out var agencyFairValueDifference);
-            decimal.TryParse(fields.ElementAtOrDefault(46), out var impairmentProvision);
-            decimal.TryParse(fields.ElementAtOrDefault(47), out var fairValueReserve);
-            decimal.TryParse(fields.ElementAtOrDefault(48), out var agencyFairValue);
-            decimal.TryParse(fields.ElementAtOrDefault(49), out var disposalsCost);
-            decimal.TryParse(fields.ElementAtOrDefault(50), out var durationInMonths);
-            decimal.TryParse(fields.ElementAtOrDefault(51), out var monthOfCompletionSinceSchemeStart);
-            decimal.TryParse(fields.ElementAtOrDefault(52), out var disposalMonthSinceCompletion);
-            DateTime.TryParse(fields.ElementAtOrDefault(53), out var imsPaymentDate);
-            bool.TryParse(fields.ElementAtOrDefault(54), out var isPaid);
-            bool.TryParse(fields.ElementAtOrDefault(55), out var isAsset);
-            decimal.TryParse(fields.ElementAtOrDefault(56), out var expectedStaircasingRate);
-            decimal.TryParse(fields.ElementAtOrDefault(57), out var estimatedSalePrice);
-            decimal.TryParse(fields.ElementAtOrDefault(58), out var regionalSaleAdjust);
-            decimal.TryParse(fields.ElementAtOrDefault(59), out var regionalStairAdjust);
-            bool.TryParse(fields.ElementAtOrDefault(60), out var notLimitedByFirstCharge);
-            decimal.TryParse(fields.ElementAtOrDefault(61), out var earlyMortgageIfNeverRepay);
-            decimal.TryParse(fields.ElementAtOrDefault(62), out var relativeSalePropertyTypeAndTenureAdjustment);
-            decimal.TryParse(fields.ElementAtOrDefault(63), out var relativeStairPropertyTypeAndTenureAdjustment);
-            bool.TryParse(fields.ElementAtOrDefault(64), out var isLondon);
-            decimal.TryParse(fields.ElementAtOrDefault(65), out var quarterSpend);
-            decimal.TryParse(fields.ElementAtOrDefault(68), out var purchasePriceBand);
-            decimal.TryParse(fields.ElementAtOrDefault(69), out var householdFiveKIncomeBand);
-            decimal.TryParse(fields.ElementAtOrDefault(70), out var householdFiftyKIncomeBand);
-            bool.TryParse(fields.ElementAtOrDefault(71), out var firstTimeBuyer);
+            int.TryParse(fields.ElementAtOrDefault(36), out var calendarYear);
+            int.TryParse(fields.ElementAtOrDefault(37), out var row);
+            int.TryParse(fields.ElementAtOrDefault(38), out var col);
+            decimal.TryParse(fields.ElementAtOrDefault(39), out var hpiStart);
+            decimal.TryParse(fields.ElementAtOrDefault(40), out var hpiEnd);
+            decimal.TryParse(fields.ElementAtOrDefault(41), out var hpiPlusMinus);
+            decimal.TryParse(fields.ElementAtOrDefault(42), out var agencyPercentage);
+            decimal.TryParse(fields.ElementAtOrDefault(43), out var mortgageEffect);
+            decimal.TryParse(fields.ElementAtOrDefault(44), out var remainingAgencyCost);
+            decimal.TryParse(fields.ElementAtOrDefault(45), out var waEstimatedPropertyValue);
+            decimal.TryParse(fields.ElementAtOrDefault(46), out var agencyFairValueDifference);
+            decimal.TryParse(fields.ElementAtOrDefault(47), out var impairmentProvision);
+            decimal.TryParse(fields.ElementAtOrDefault(48), out var fairValueReserve);
+            decimal.TryParse(fields.ElementAtOrDefault(49), out var agencyFairValue);
+            decimal.TryParse(fields.ElementAtOrDefault(50), out var disposalsCost);
+            decimal.TryParse(fields.ElementAtOrDefault(51), out var durationInMonths);
+            decimal.TryParse(fields.ElementAtOrDefault(52), out var monthOfCompletionSinceSchemeStart);
+            decimal.TryParse(fields.ElementAtOrDefault(53), out var disposalMonthSinceCompletion);
+            DateTime.TryParse(fields.ElementAtOrDefault(54), out var imsPaymentDate);
+            bool.TryParse(fields.ElementAtOrDefault(55), out var isPaid);
+            bool.TryParse(fields.ElementAtOrDefault(58), out var isAsset);
+            decimal.TryParse(fields.ElementAtOrDefault(59), out var expectedStaircasingRate);
+            decimal.TryParse(fields.ElementAtOrDefault(60), out var estimatedSalePrice);
+            decimal.TryParse(fields.ElementAtOrDefault(61), out var regionalSaleAdjust);
+            decimal.TryParse(fields.ElementAtOrDefault(63), out var regionalStairAdjust);
+            bool.TryParse(fields.ElementAtOrDefault(64), out var notLimitedByFirstCharge);
+            decimal.TryParse(fields.ElementAtOrDefault(65), out var earlyMortgageIfNeverRepay);
+            decimal.TryParse(fields.ElementAtOrDefault(66), out var relativeSalePropertyTypeAndTenureAdjustment);
+            decimal.TryParse(fields.ElementAtOrDefault(69), out var relativeStairPropertyTypeAndTenureAdjustment);
+            bool.TryParse(fields.ElementAtOrDefault(70), out var isLondon);
+            decimal.TryParse(fields.ElementAtOrDefault(71), out var quarterSpend);
+            decimal.TryParse(fields.ElementAtOrDefault(72), out var purchasePriceBand);
+            decimal.TryParse(fields.ElementAtOrDefault(73), out var householdFiveKIncomeBand);
+            decimal.TryParse(fields.ElementAtOrDefault(74), out var householdFiftyKIncomeBand);
+            bool.TryParse(fields.ElementAtOrDefault(75), out var firstTimeBuyer);
             var createAssetRequest = new CreateAssetRequest
             {
                 Programme = fields.ElementAtOrDefault(0),
@@ -143,8 +143,8 @@ namespace HomesEngland.UseCase.CreateAsset.Models.Factory
                 RelativeStairPropertyTypeAndTenureAdjustment = relativeStairPropertyTypeAndTenureAdjustment,
                 IsLondon = isLondon,
                 QuarterSpend = quarterSpend,
-                MortgageProvider = fields.ElementAtOrDefault(66),
-                HouseType = fields.ElementAtOrDefault(67),
+                MortgageProvider = fields.ElementAtOrDefault(67),
+                HouseType = fields.ElementAtOrDefault(68),
                 PurchasePriceBand = purchasePriceBand,
                 HouseholdFiveKIncomeBand = householdFiveKIncomeBand,
                 HouseholdFiftyKIncomeBand = householdFiftyKIncomeBand,
