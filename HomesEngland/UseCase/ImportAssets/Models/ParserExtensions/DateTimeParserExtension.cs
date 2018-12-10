6e9220000b44d3d1dbd5144f5980367eb80e4bd4
Jeff Pinkham <jeff@pinkhamsoftware.com>
2018-12-10 17:06:31 +0000

    Create DateTimeParserExtension

---
+++ HomesEngland/UseCase/ImportAssets/Models/ParserExtensions/DateTimeParserExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomesEngland.UseCase.ImportAssets.Models.ParserExtensions
+{
+    public static class DateTimeParserExtension
+    {
+        //public static DateTime? TryParseNullable(this string )
+    }
+}
