ff6ca502a13713ff278105d05ebba14c19608a40
Daniel Burnley <dan@danops.uk>
2018-12-11 23:02:32 +0000

    Add tests around booleans - begin test for all other fields

--- HomesEnglandTest/Domain/CreateAssetRequestFactoryTests.cs
+++ HomesEnglandTest/Domain/CreateAssetRequestFactoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Bogus.DataSets;
 using FluentAssertions;
 using HomesEngland.Domain.Factory;
 using HomesEngland.UseCase.CreateAsset.Models;
@@ -42,9 +43,12 @@ namespace HomesEnglandTest.Domain
             asset.Should().BeNull();
         }
 
-        [TestCase(";", "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
-        [TestCase(";", "Old Scheme ;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
-        [TestCase(";", "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
+        [TestCase(";",
+            "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
+        [TestCase(";",
+            "Old Scheme ;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
+        [TestCase(";",
+            "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
         public void GivenValidInput_WhenWeCallCreate_ThenAssetHasFieldsPopulated(string delimiter, string csvLine)
         {
             //arrange
@@ -133,7 +137,7 @@ namespace HomesEnglandTest.Domain
             asset.HouseholdFiveKIncomeBand.Should().NotBeNull();
             asset.HouseholdFiftyKIncomeBand.Should().NotBeNull();
             asset.FirstTimeBuyer.Should().NotBeNull();
-    }
+        }
 
         [TestCase("Help to buy")]
         [TestCase("")]
@@ -143,7 +147,7 @@ namespace HomesEnglandTest.Domain
 
             asset.Programme.Should().BeEquivalentTo(input);
         }
-        
+
         [TestCase("Homes England")]
         [TestCase("")]
         public void GivenEquityOwner_ThenAssetHasFieldPopulatedCorrectly(string input)
@@ -152,7 +156,7 @@ namespace HomesEnglandTest.Domain
 
             asset.EquityOwner.Should().BeEquivalentTo(input);
         }
-        
+
         [TestCase("1001", 1001)]
         [TestCase("2002", 2002)]
         public void GivenSchemeID_ThenAssetHasFieldPopulatedCorrectly(string input, int expectedSchemeId)
@@ -161,7 +165,7 @@ namespace HomesEnglandTest.Domain
 
             asset.SchemeId.Should().Be(expectedSchemeId);
         }
-        
+
         [TestCase("LocationLAOne")]
         [TestCase("LocationLATwo")]
         public void GivenLocationLARegionName_ThenAssetHasFieldPopulatedCorrectly(string input)
@@ -170,7 +174,56 @@ namespace HomesEnglandTest.Domain
 
             asset.LocationLaRegionName.Should().BeEquivalentTo(input);
         }
+
+        [TestCase("Paid", true)]
+        [TestCase("  Paid  ", true)]
+        [TestCase("PaId", true)]
+        [TestCase("NotPaid", false)]
+        [TestCase("", false)]
+        public void GivenPaidField_ThenIsPaidSetToCorrectValue(string input, bool expectedIsPaid)
+        {
+            var asset = CreateAssetRequestForColumn(input, 55);
+
+            asset.IsPaid.Should().Be(expectedIsPaid);
+        }
         
+        [TestCase("Asset", true)]
+        [TestCase("  Asset  ", true)]
+        [TestCase("AsSeT", true)]
+        [TestCase("NotAnAsset", false)]
+        [TestCase("", false)]
+        public void GivenAssetField_ThenIsAssetSetToCorrectValue(string input, bool expectedIsAsset)
+        {
+            var asset = CreateAssetRequestForColumn(input, 56);
+
+            asset.IsAsset.Should().Be(expectedIsAsset);
+        }
+        
+        [TestCase("Non-London", false)]
+        [TestCase("  Non-London  ", false)]
+        [TestCase("  NoN-lOnDoN  ", false)]
+        [TestCase("London", true)]
+        [TestCase("", true)]
+        public void GivenNonLondonField_ThenIsLondonSetToCorrectValue(string input, bool expectedIsLondon)
+        {
+            var asset = CreateAssetRequestForColumn(input, 69);
+
+            asset.IsLondon.Should().Be(expectedIsLondon);
+        }
+        
+        [TestCase("Y", true)]
+        [TestCase("  Y  ", true)]
+        [TestCase("y", true)]
+        [TestCase("nah", false)]
+        [TestCase("", false)]
+        public void GivenFirstTimeBuyer_ThenFirstTimeBuyerSetToCorrectValue(string input, bool expectedIsFirstTimeBuyer)
+        {
+            var asset = CreateAssetRequestForColumn(input, 77);
+
+            asset.FirstTimeBuyer.Should().Be(expectedIsFirstTimeBuyer);
+        }
+        
+
         private CreateAssetRequest CreateAssetRequestForColumn(string input, int csvColumn)
         {
             List<string> values = new List<string>();
@@ -191,11 +244,10 @@ namespace HomesEnglandTest.Domain
             return asset;
         }
 
-        [Ignore("Meow")]
-        [TestCase(";", "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;#N/A;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; - ; 110,250 ; 147,000 ; 600 ; - ; 30,000 ;15-May-17;20.0000%;;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
-        [TestCase(";", "Old Scheme ;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;#N/A;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; - ; 110,250 ; 147,000 ; 600 ; - ; 30,000 ;15-May-17;20.0000%;;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
-        [TestCase(";", "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;#N/A;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; - ; 110,250 ; 147,000 ; 600 ; - ; 30,000 ;15-May-17;20.0000%;;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
-        public void GivenValidInput_WhenWeCallCreate_ThenAssetHasFieldsPopulatedInOrder(string delimiter, string csvLine)
+        [TestCase(";",
+            "Help to Buy;Homes England;583417;Midlands;West Midlands; 2 ; Primrose Gardens Plot 14 30 Mews Mews Netherton  Dudley DY2 9LD ; Primrose Gardens Plot 14 ; 30 ; Mews Mews ; Cat Town, DogVille ; DY2 9LD ; Taylor Wimpey ; Orbit Group Limited ;26-Jul-13;2-Sep-13;26-Jul-13;27-Jul-13;26-Jul-13; 7,350 ; 29,400 ; 1000 ;100%; 2 ; 110,250 ; 147,000 ; 600 ; 10,000 ; 30,000 ;15-May-17;20.0000%; 20.0% ;0.0000%;0;7;2013;7/2013;99;13;94.7;125.4;32.4%;20.0%;0.000%;- ;- ;- ;- ;- ;- ;(30,000.00);63;3;46;11-Sep-13;Paid;Asset;Semi-detached;Freehold;42%; 179,332 ; 176,939 ;-7.84%;-9.07%;;0.00%;;0.00%;0.00%;Non-London;2;Halifax;Semi-detached; 200,000 ; 28,842 ; 30,000 ; 50,000 ; Y ")]
+        public void GivenValidInput_WhenWeCallCreate_ThenAssetHasFieldsPopulatedInOrder(string delimiter,
+            string csvLine)
         {
             //arrange
             var csvAsset = new CsvAsset
@@ -225,65 +277,65 @@ namespace HomesEnglandTest.Domain
             asset.CompletionDateForHpiStart.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(14)));
             asset.ImsActualCompletionDate.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(15)));
             asset.ImsExpectedCompletionDate.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(16)));
-            asset.ImsLegalCompletionDate.Should().BeNull();
+            asset.ImsLegalCompletionDate.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(17)));
             asset.HopCompletionDate.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(18)));
-            asset.Deposit.Should().NotBeNull();
-            asset.AgencyEquityLoan.Should().NotBeNull();
-            asset.DeveloperEquityLoan.Should().NotBeNull();
-            asset.ShareOfRestrictedEquity.Should().NotBeNull();
-            asset.DeveloperDiscount.Should().NotBeNull();
-            asset.Mortgage.Should().NotBeNull();
-            asset.PurchasePrice.Should().NotBeNull();
-            asset.Fees.Should().NotBeNull();
-            asset.HistoricUnallocatedFees.Should().NotBeNull();
-            asset.ActualAgencyEquityCostIncludingHomeBuyAgentFee.Should().NotBeNull();
-            asset.FullDisposalDate.Should().NotBeNull();
-            asset.OriginalAgencyPercentage.Should().NotBeNull();
-            asset.StaircasingPercentage.Should().NotBeNull();
-            asset.NewAgencyPercentage.Should().NotBeNull();
-            asset.Invested.Should().NotBeNull();
-            asset.Month.Should().NotBeNull();
-            asset.CalendarYear.Should().NotBeNull();
-            asset.MMYYYY.Should().NotBeNull();
-            asset.Row.Should().NotBeNull();
-            asset.Col.Should().NotBeNull();
-            asset.HPIStart.Should().NotBeNull();
-            asset.HPIEnd.Should().NotBeNull();
-            asset.HPIPlusMinus.Should().NotBeNull();
-            asset.AgencyPercentage.Should().NotBeNull();
-            asset.MortgageEffect.Should().NotBeNull();
-            asset.RemainingAgencyCost.Should().NotBeNull();
-            asset.WAEstimatedPropertyValue.Should().NotBeNull();
-            asset.AgencyFairValueDifference.Should().NotBeNull();
-            asset.ImpairmentProvision.Should().NotBeNull();
-            asset.FairValueReserve.Should().NotBeNull();
-            asset.AgencyFairValue.Should().NotBeNull();
-            asset.DisposalsCost.Should().NotBeNull();
-            asset.DurationInMonths.Should().NotBeNull();
-            asset.MonthOfCompletionSinceSchemeStart.Should().NotBeNull();
-            asset.DisposalMonthSinceCompletion.Should().NotBeNull();
-            asset.IMSPaymentDate.Should().NotBeNull();
-            asset.IsPaid.Should().NotBeNull();
-            asset.IsAsset.Should().NotBeNull();
-            asset.PropertyType.Should().NotBeNull();
-            asset.Tenure.Should().NotBeNull();
-            asset.ExpectedStaircasingRate.Should().NotBeNull();
-            asset.EstimatedSalePrice.Should().NotBeNull();
-            asset.RegionalSaleAdjust.Should().NotBeNull();
-            asset.RegionalStairAdjust.Should().NotBeNull();
-            asset.NotLimitedByFirstCharge.Should().NotBeNull();
-            asset.EarlyMortgageIfNeverRepay.Should().NotBeNull();
-            asset.ArrearsEffectAppliedOrLimited.Should().NotBeNull();
-            asset.RelativeSalePropertyTypeAndTenureAdjustment.Should().NotBeNull();
-            asset.RelativeStairPropertyTypeAndTenureAdjustment.Should().NotBeNull();
-            asset.IsLondon.Should().NotBeNull();
-            asset.QuarterSpend.Should().NotBeNull();
-            asset.MortgageProvider.Should().NotBeNull();
-            asset.HouseType.Should().NotBeNull();
-            asset.PurchasePriceBand.Should().NotBeNull();
-            asset.HouseholdFiveKIncomeBand.Should().NotBeNull();
-            asset.HouseholdFiftyKIncomeBand.Should().NotBeNull();
-            asset.FirstTimeBuyer.Should().NotBeNull();
+            asset.Deposit.Should().Be(7350);
+            asset.AgencyEquityLoan.Should().Be(29400);
+            asset.DeveloperEquityLoan.Should().Be(1000);
+            asset.ShareOfRestrictedEquity.Should().Be(100);
+            asset.DeveloperDiscount.Should().Be(2);
+            asset.Mortgage.Should().Be(110250);
+            asset.PurchasePrice.Should().Be(147000);
+            asset.Fees.Should().Be(600);
+            asset.HistoricUnallocatedFees.Should().Be(10000);
+            asset.ActualAgencyEquityCostIncludingHomeBuyAgentFee.Should().Be(30000);
+            asset.FullDisposalDate.Should().Be(DateTime.Parse(fields.ElementAtOrDefault(29)));
+            asset.OriginalAgencyPercentage.Should().Be(20);
+            asset.StaircasingPercentage.Should().Be(20);
+            asset.NewAgencyPercentage.Should().Be(0);
+            asset.Invested.Should().Be(0);
+            asset.Month.Should().Be(7);
+            asset.CalendarYear.Should().Be(2013);
+            asset.MMYYYY.Should().Be("MMYYYY");
+            asset.Row.Should().Be(123);
+            asset.Col.Should().Be(123);
+            asset.HPIStart.Should().Be(123);
+            asset.HPIEnd.Should().Be(123);
+            asset.HPIPlusMinus.Should().Be(123);
+            asset.AgencyPercentage.Should().Be(123);
+            asset.MortgageEffect.Should().Be(123);
+            asset.RemainingAgencyCost.Should().Be(123);
+            asset.WAEstimatedPropertyValue.Should().Be(123);
+            asset.AgencyFairValueDifference.Should().Be(123);
+            asset.ImpairmentProvision.Should().Be(123);
+            asset.FairValueReserve.Should().Be(123);
+            asset.AgencyFairValue.Should().Be(123);
+            asset.DisposalsCost.Should().Be(123);
+            asset.DurationInMonths.Should().Be(123);
+            asset.MonthOfCompletionSinceSchemeStart.Should().Be(123);
+            asset.DisposalMonthSinceCompletion.Should().Be(123);
+            asset.IMSPaymentDate.Should().Be(DateTime.Now);
+            asset.IsPaid.Should().Be(true);
+            asset.IsAsset.Should().Be(true);
+            asset.PropertyType.Should().Be("Property type");
+            asset.Tenure.Should().Be("Tenure");
+            asset.ExpectedStaircasingRate.Should().Be(123);
+            asset.EstimatedSalePrice.Should().Be(123);
+            asset.RegionalSaleAdjust.Should().Be(123);
+            asset.RegionalStairAdjust.Should().Be(123);
+            asset.NotLimitedByFirstCharge.Should().Be(true);
+            asset.EarlyMortgageIfNeverRepay.Should().Be(123);
+            asset.ArrearsEffectAppliedOrLimited.Should().Be("Yes?");
+            asset.RelativeSalePropertyTypeAndTenureAdjustment.Should().Be(123);
+            asset.RelativeStairPropertyTypeAndTenureAdjustment.Should().Be(123);
+            asset.IsLondon.Should().Be(true);
+            asset.QuarterSpend.Should().Be(123);
+            asset.MortgageProvider.Should().Be("Mortgage provider");
+            asset.HouseType.Should().Be("House type");
+            asset.PurchasePriceBand.Should().Be(123);
+            asset.HouseholdFiveKIncomeBand.Should().Be(123);
+            asset.HouseholdFiftyKIncomeBand.Should().Be(123);
+            asset.FirstTimeBuyer.Should().Be(true);
         }
     }
 }
